bd5ecb903385e024329d9618609e4557fcc72ab4
markmipt <markmipt@gmail.com>
2014-05-19 16:04:16 +0400

    fix for PEP8 style and move to module structure

--- tandem2xml/tandem2xml.py
+++ tandem2xml/tandem2xml.py
@@ -1,13 +1,18 @@
 from pyteomics import tandem
-from sys import argv
 import jinja2
 
-parameters = dict()
-params = tandem.iterfind(argv[1], 'group[type=parameters]', recursive=True)
-for param in params:
-    parameters[param['label']] = {v['label']: (v['note'] if 'note' in v else "") for v in param['note']}
 
-templateLoader = jinja2.FileSystemLoader(searchpath="../templates/")
-templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "template.jinja"
-template = templateEnv.get_template(TEMPLATE_FILE)
+def convert(path_to_file):
+    parameters = dict()
+    params = tandem.iterfind(argv[1], 'group[type=parameters]', recursive=True)
+    for param in params:
+        parameters[param['label']] = {v['label']: (v['note'] if 'note' in v else "") for v in param['note']}
+
+    templateloader = jinja2.FileSystemLoader(searchpath="../templates/")
+    templateenv = jinja2.Environment(loader=templateloader)
+    template_file = "template.jinja"
+    template = templateenv.get_template(template_file)
+
+if __name__ == '__main__':
+    from sys import argv
+    convert(argv[1])
