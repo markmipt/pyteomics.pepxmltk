c74e7723284a56f7b81310a54d8d84a2e009ea77
Mark Ivanov <markmipt@users.noreply.github.com>
2014-05-21 16:47:58 +0400

    add writing of modifications information

--- tandem2xml/tandem2xml.py
+++ tandem2xml/tandem2xml.py
@@ -1,5 +1,128 @@
 from pyteomics import tandem, parser, mass
+from copy import copy
 import jinja2
+
+
+class Modifications():
+    def __init__(self, path_to_file, input_parameters):
+        self.modifications = []
+        self.std_aa_mass = copy(mass.std_aa_mass)
+        self.fixed_mods = []
+        self.variable_mods = []
+        self.std_aa_mass['N_term'] = float(input_parameters['protein, cleavage N-terminal mass change'])
+        self.std_aa_mass['C_term'] = float(input_parameters['protein, cleavage C-terminal mass change'])
+        for psm_tandem in tandem.read(path_to_file):
+            for mod in psm_tandem['protein'][0]['peptide'].get('aa', []):
+                modification = dict()
+                modification['aminoacid'] = mod['type']
+                modification['massdiff'] = mod['modified']
+
+                if mod['at'] == psm_tandem['protein'][0]['peptide']['start']:
+                    modification['terminus'] = 'N'
+                elif mod['at'] == psm_tandem['protein'][0]['peptide']['end']:
+                    modification['terminus'] = 'C'
+                else:
+                    modification['terminus'] = None
+
+                modification['variable'] = 'Y'
+                if modification not in self.modifications:
+                    self.modifications.append(modification)
+        self.get_modifications_from_params(input_parameters)
+        self.variable_info()
+        self.group_terminus()
+        self.remove_dupcated_modifications()
+        self.drop_wrong_terminus()
+        self.sort_modifications()
+        self.change_std_aa_mass()
+        self.calculate_modification_masses()
+        self.info_about_xtandem_term_modifications()
+        self.add_lowercase_for_term_modifications()
+
+    def get_modifications_from_params(self, input_parameters):
+        for mod in input_parameters['residue, potential modification mass'].split(','):
+            self.variable_mods.append((mod.split('@')))
+        for mod in input_parameters['residue, modification mass'].split(','):
+            self.fixed_mods.append((mod.split('@')))
+        if input_parameters.get('protein, quick acetyl', 'yes') == 'yes':
+            self.variable_mods.append(['42.010565', '['])
+        if input_parameters['refine'] == 'yes':
+            for mod in input_parameters['refine, potential modification mass'].split(',')\
+                    + input_parameters['refine, potential N-terminus modifications'].split(',')\
+                    + input_parameters['refine, potential C-terminus modifications'].split(','):
+                if (mod.split('@')) not in self.variable_mods:
+                    self.variable_mods.append((mod.split('@')))
+            for mod in input_parameters['refine, modification mass'].split(','):
+                if (mod.split('@')) not in self.fixed_mods:
+                    self.fixed_mods.append((mod.split('@')))
+
+    @staticmethod
+    def is_equal_modification(modification1, modification2):
+        if isinstance(modification2, list):
+            if round(modification1['massdiff'] - float(modification2[0]), 4) == 0:
+                if (not modification1['terminus'] and modification1['aminoacid'] == modification2[1]) \
+                    or (modification1['terminus'] == 'N' and modification2[1] == '[') \
+                        or (modification1['terminus'] == 'C' and modification2[1] == ']'):
+                            return True
+        elif isinstance(modification2, dict):
+            if modification1['aminoacid'] == modification2['aminoacid'] \
+                and modification1['massdiff'] == modification2['massdiff']:
+                return True
+
+    def sort_modifications(self):
+        self.modifications = sorted(self.modifications, key=lambda item: item['variable'])
+
+    def change_std_aa_mass(self):
+        for modification in self.modifications:
+            if modification['variable'] == 'N':
+                if modification['aminoacid']:
+                    self.std_aa_mass[modification['aminoacid']] += modification['massdiff']
+            else:
+                break
+
+    def calculate_modification_masses(self):
+        for modification in self.modifications:
+            if modification['aminoacid']:
+                modification['mass'] = round(self.std_aa_mass[modification['aminoacid']]\
+                                       + (modification['massdiff'] if modification['variable'] == 'Y' else 0), 5)
+            else:
+                modification['mass'] = round(self.std_aa_mass['%s_term' % (modification['terminus'], )]\
+                                       + (modification['massdiff'] if modification['variable'] == 'Y' else 0), 5)
+
+    def info_about_xtandem_term_modifications(self):
+        for modification in self.modifications:
+            if modification['terminus'] and modification['aminoacid']:
+                modification['close_label'] = ' symbol="^" /><!--X! Tandem %s-terminal AA variable modification-->'\
+                                        % (modification['terminus'].lower())
+            else:
+                modification['close_label'] = ' />'
+
+    def remove_dupcated_modifications(self):
+        self.modifications = [dict(t) for t in set([tuple(d.items()) for d in self.modifications])]
+
+    def drop_wrong_terminus(self):
+        for modification_copy in list(self.modifications):
+            if not modification_copy['terminus']:
+                for modification in list(self.modifications):
+                    if modification['terminus'] and self.is_equal_modification(modification_copy, modification):
+                        self.modifications.remove(modification)
+
+    def variable_info(self):
+        for modification in self.modifications:
+            for mod in self.fixed_mods:
+                if self.is_equal_modification(modification, mod):
+                    modification['variable'] = 'N'
+                    break
+
+    def group_terminus(self):
+        for modification in self.modifications:
+            if modification['terminus'] and any(self.is_equal_modification(modification, mod)
+                                                for mod in self.fixed_mods + self.variable_mods):
+                modification['aminoacid'] = None
+
+    def add_lowercase_for_term_modifications(self):
+        for modification in self.modifications:
+            if modification['terminus']:
+                modification['terminus_lower'] = modification['terminus'].lower()
 
 
 class Psm:
@@ -103,6 +226,7 @@ def convert(path_to_file, path_to_output):
     for param in params:
         parameters[param['label']] = {v['label']: (v['note'] if 'note' in v else "") for v in param['note']}
     proteases = (Protease(rule) for rule in parameters['input parameters']['protein, cleavage site'].split(','))
+    modifications = Modifications(path_to_file, parameters['input parameters'])
     psms = (Psm(psm_tandem) for psm_tandem in tandem.read(path_to_file))
     templateloader = jinja2.FileSystemLoader(searchpath="../templates/")
     templateenv = jinja2.Environment(loader=templateloader)
@@ -112,7 +236,8 @@ def convert(path_to_file, path_to_output):
     templatevars = {'parameters': parameters,
                     'proteases': proteases,
                     'path_to_file': path_to_file,
-                    'modifications': [],
+                    'modifications': [m for m in modifications.modifications if m['aminoacid']],
+                    'term_modifications': [m for m in modifications.modifications if not m['aminoacid']],
                     'psms': psms
     }
     output = open(path_to_output, 'w')
