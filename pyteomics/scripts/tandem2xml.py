a2f81fe83a4818c1f503b3fb51a5eaa6dee65634
Mark Ivanov <markmipt@users.noreply.github.com>
2014-07-31 18:18:32 +0400

    add merging and filtering options

--- pyteomics/scripts/tandem2xml.py
+++ pyteomics/scripts/tandem2xml.py
@@ -3,4 +3,12 @@ from pyteomics.tandem2xml import convert
 from os import path
 from sys import argv
 
-convert(path.abspath(argv[1]), path.abspath(argv[2]))
+try:
+    fdr = float(argv[-1])
+    out_file = argv[-2]
+    files = [path.abspath(f) for f in argv[1:-2]]
+except:
+    fdr = None
+    out_file = argv[-1]
+    files = [path.abspath(f) for f in argv[1:-1]]
+convert(files, path.abspath(out_file), fdr)
