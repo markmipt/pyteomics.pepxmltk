edca746f4d2257a9da41dac9d41fce37a1c12163
Mark Ivanov <markmipt@gmail.com>
2014-08-08 14:10:59 +0400

    add parameters parsing and help

--- pyteomics/scripts/pepxmltk.py
+++ pyteomics/scripts/pepxmltk.py
@@ -1,14 +1,15 @@
 #! python
 from pyteomics.pepxmltk import convert
 from os import path
-from sys import argv
+import argparse
 
-try:
-    fdr = float(argv[-1]) / 100
-    out_file = argv[-2]
-    files = [path.abspath(f) for f in argv[1:-2]]
-except:
-    fdr = None
-    out_file = argv[-1]
-    files = [path.abspath(f) for f in argv[1:-1]]
-convert(files, path.abspath(out_file), fdr)
+parser = argparse.ArgumentParser()
+parser.add_argument('files', nargs='*', help='list of pepXML or tXML files')
+parser.add_argument('-o', required=True, help='path to output file')
+parser.add_argument('-f', default=None, help='fdr, %%')
+args =  parser.parse_args()
+
+fdr = args.f
+if fdr:
+    fdr /= 100
+convert(args.files, path.abspath(args.o), fdr=fdr)
