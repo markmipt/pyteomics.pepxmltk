cbb0d487b8f771b1bd8bfc181b3c4ac6e47343a1
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2015-10-06 19:24:39 +0300

    Add missing __init__.py, update help

--- pyteomics/scripts/pepxmltk.py
+++ pyteomics/scripts/pepxmltk.py
@@ -3,10 +3,28 @@ from pyteomics.pepxmltk import convert
 from os import path
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('files', nargs='*', help='list of pepXML or tXML files')
-parser.add_argument('-o', help='path to output file')
-parser.add_argument('-f', default=None, help='fdr, %%')
+parser = argparse.ArgumentParser(
+    description='Convert X!Tandem files to pepXML, or filter pepXML files.',
+    epilog='''
+
+Example usage
+-------------
+
+pepxmltk.py input.t.xml output.pep.xml # convert single file to pepXML
+
+pepxmltk.py input1.t.xml input2.t.xml ... inputN.t.xml # convert multiple files,
+    #   derive names from input names
+
+pepxmltk.py input1.t.xml input2.t.xml ... inputN.t.xml --out output.pep.xml
+    # convert multiple files, join results into a single pepXML file
+
+pepxmltk.py input1.pep.xml input2.pep.xml ... inputN.pep.xml output.pep.xml --fdr 1
+    # join multiple pepXML files and filter them to 1% FDR
+    ''',
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('files', nargs='+', help='list of pepXML or tXML files')
+parser.add_argument('-o', '--out', help='path to output file')
+parser.add_argument('-f', '--fdr', default=None, help='FDR, %%')
 args = parser.parse_args()
 
 fdr = args.f
